a1b42513602942e5755603b3007a73830cf296d8
ZhukDima <zhuk@gmail.com>
2021-12-17 22:31:28 +0300

    Scripted auto-commit on change (2021-12-17 22:31:28) by gitwatch.sh

--- src/main/java/ZooKeeperWatcher.java
+++ src/main/java/ZooKeeperWatcher.java
@@ -1,5 +1,25 @@
+import akka.actor.ActorRef;
+import org.apache.zookeeper.KeeperException;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
-public class ZooKeeperWatcher extends Watcher {
-    
+import java.util.ArrayList;
+
+public class ZooKeeperWatcher implements Watcher {
+    private final ZooKeeper zooKeeper;
+    private final ActorRef actorConfig;
+
+    ZooKeeperWatcher(ZooKeeper zooKeeper, ActorRef actorConfig)
+            throws InterruptedException, KeeperException {
+        this.actorConfig = actorConfig;
+        this.zooKeeper = zooKeeper;
+
+        byte[] data = this.zooKeeper.getData("/servers", true, null);
+        System.out.printf("servers data=%s", new String(data));
+    }
+
+    private void sendServers() {
+        ArrayList<String> servers = new ArrayList<>();
+        zooKeeper.getChildren()
+    }
 }
