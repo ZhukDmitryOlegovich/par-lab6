d2d614af06a792f4b3c0c6ee515ade4c8bfd387f
ZhukDima <zhuk@gmail.com>
2021-12-17 22:16:04 +0300

    Scripted auto-commit on change (2021-12-17 22:16:04) by gitwatch.sh

--- src/main/java/ActorConfig.java
+++ src/main/java/ActorConfig.java
@@ -1,2 +1,20 @@
-package PACKAGE_NAME;public class ActorConfig {
+import akka.actor.AbstractActor;
+
+import java.util.ArrayList;
+import java.util.Random;
+
+public class ActorConfig extends AbstractActor {
+    private ArrayList<String> servers = new ArrayList<>();
+
+    private final Random random = new Random();
+
+    @Override
+    public Receive createReceive() {
+        return receiveBuilder()
+                .build();
+    }
+
+    public String getRandomServerPort() {
+        return servers.get(random.nextInt(servers.size()));
+    }
 }
