1331b97d11bd42d433be16190c24cbde85c5b464
ZhukDima <zhuk@gmail.com>
2021-12-17 22:36:00 +0300

    Scripted auto-commit on change (2021-12-17 22:36:00) by gitwatch.sh

--- src/main/java/ZooKeeperWatcher.java
+++ src/main/java/ZooKeeperWatcher.java
@@ -23,7 +23,7 @@ public class ZooKeeperWatcher implements Watcher {
     private void sendServers() throws InterruptedException, KeeperException {
         ArrayList<String> servers = new ArrayList<>();
         zooKeeper.getChildren(SERVERS_PATH, this).stream().map(
-                
+                s -> new String()
         )
     }
 }
