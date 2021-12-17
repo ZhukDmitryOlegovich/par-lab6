c16c6fb9bc778e1df465ee5fdb76ad1b8dabf82e
ZhukDima <zhuk@gmail.com>
2021-12-17 22:49:30 +0300

    Scripted auto-commit on change (2021-12-17 22:49:29) by gitwatch.sh

--- src/main/java/HttpServer.java
+++ src/main/java/HttpServer.java
@@ -5,5 +5,5 @@ public class HttpServer implements Watcher {
     private static final String PATH_SERVERS = "localhost:";
     private static final String URL_QUERY_PARAM = "url";
     private static final String COUNT_QUERY_PARAM = "count";
-    private static final String
+    private static final String ZERO_COUNT_STRING = "0";
 }
