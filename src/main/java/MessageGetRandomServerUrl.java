9126b5b881c83a6760fb930389f82efa2fd78577
ZhukDima <zhuk@gmail.com>
2021-12-17 22:20:57 +0300

    Scripted auto-commit on change (2021-12-17 22:20:56) by gitwatch.sh

--- src/main/java/MessageGetRandomServerUrl.java
+++ src/main/java/MessageGetRandomServerUrl.java
@@ -1,3 +1,4 @@
 public class MessageGetRandomServerUrl {
-    public MessageGetRandomServerUrl() {}
+    MessageGetRandomServerUrl() {
+    }
 }
--- src/main/java/MessageSendServersList.java
+++ src/main/java/MessageSendServersList.java
@@ -2,4 +2,12 @@ import java.util.ArrayList;
 
 public class MessageSendServersList {
     private final ArrayList<String> servers;
+
+    MessageSendServersList(ArrayList<String> servers) {
+        this.servers = servers;
+    }
+
+    public ArrayList<String> getServers() {
+        return servers;
+    }
 }
