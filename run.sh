6663f7dcaa2e6219cf3db10cf31b7f255c6ee8bb
ZhukDima <zhuk@gmail.com>
2021-12-17 20:27:05 +0300

    Scripted auto-commit on change (2021-12-17 20:27:05) by gitwatch.sh

--- run.sh
+++ run.sh
@@ -1,3 +1,3 @@
 curl -X GET --fail 'http://localhost:8080/?testUrl=http://ya.ru&count=20'
 curl -X GET --fail 'http://localhost:8080/?testUrl=http://clck.ru&count=20'
-#curl -X GET --fail 'http://localhost:8080/?testUrl=http://rambler.ru&count=20'
+##curl -X GET --fail 'http://localhost:8080/?testUrl=http://rambler.ru&count=20'
