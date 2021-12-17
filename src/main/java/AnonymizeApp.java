56c0656cc20c13622cea6cc6791fd14e74013c76
ZhukDima <zhuk@gmail.com>
2021-12-17 22:13:42 +0300

    Scripted auto-commit on change (2021-12-17 22:13:42) by gitwatch.sh

--- src/main/java/AnonymizeApp.java
+++ src/main/java/AnonymizeApp.java
@@ -1,5 +1,19 @@
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.Props;
+import org.apache.log4j.BasicConfigurator;
+
+import java.util.Arrays;
+
 public class AnonymizeApp {
     public static void main(String[] args) {
-
+        if (args.length < 2) {
+            System.err.println("Usage: need ports >2");
+            System.exit(-1);
+        }
+        BasicConfigurator.configure();
+        System.out.println("Start! Ports: " + Arrays.toString(args));
+        ActorSystem system = ActorSystem.create("lab6");
+        ActorRef actorConfig = system.actorOf(Props.create());
     }
 }
